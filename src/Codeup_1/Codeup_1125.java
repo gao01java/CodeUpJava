7a25fc5e3040511b0052a9ad20c835e6ae351f9d
gao01java <60835579+gao01java@users.noreply.github.com>
2021-05-09 16:09:02 +0900

    HelloWorld

--- src/Codeup_1/Codeup_1125.java
+++ src/Codeup_1/Codeup_1125.java
@@ -6,6 +6,8 @@ public class Codeup_1125 {
     public static void main(String[]args){
         Scanner in=new Scanner(System.in);
 
-
+        int a=in.nextInt();
+        System.out.printf("%o ",a);
+        System.out.printf("%X",a);
     }
 }
