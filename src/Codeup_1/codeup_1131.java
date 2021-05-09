324be63f47a1a835d317c01de368a8ec48a4cd42
gao01java <60835579+gao01java@users.noreply.github.com>
2021-05-09 16:21:34 +0900

    Hello
    123

---
+++ src/Codeup_1/codeup_1131.java
@@ -0,0 +1,13 @@
+package Codeup_1;
+
+import java.util.Scanner;
+
+public class codeup_1131 {
+    public static void main(String[]args){
+        Scanner in=new Scanner(System.in);
+
+        String a=in.nextLine();
+
+        System.out.println(a);
+    }
+}
